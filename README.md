89a9d258f2ce39bb645f922dce340a22229f1ea2
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-05-23 10:16:13 +0700

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# KEVINNITRO LINUX DOTFILES
+
+[![GitHub last commit (by committer)](https://img.shields.io/github/last-commit/KevinNitroG/linux-dotfiles?style=for-the-badge&color=FAB387)](../../commits/main)
+![GitHub repo size](https://img.shields.io/github/repo-size/KevinNitroG/linux-dotfiles?style=for-the-badge&color=B4BEFE)
+
+---
+
+## FCITX5
+
+- Set evironment variables in `.zshrc`
+  ```sh
+  export XMODIFIERS=@im=fcitx
+  export GTK_IM_MODULE=fcitx
+  export QT_IM_MODULE=fcitx
+  ```
+
+- GTK setup ...
+
+> [!NOTE]
+>
+> Ref: [Fcitx 5 Setup](https://fcitx-im.org/wiki/Setup_Fcitx)
---
+++ dot_install/packages.sh
@@ -0,0 +1,167 @@
+#!/usr/bin/env bash
+
+# Source: https://github.com/Lissy93/dotfiles/blob/master/scripts/installs/arch-pacman.sh
+
+################################################################
+# 📜 Arch Linux, Pacman Package Install / Update Script        #
+################################################################
+# Installs listed packages on Arch-based systems via Pacman    #
+# Also updates the cache database and existing applications    #
+# Confirms apps aren't installed via different package manager #
+# Doesn't include desktop apps, that're managed via Flatpak    #
+# Apps are sorted by category, and arranged alphabetically     #
+# Be sure to delete / comment out anything you do not need     #
+# For more info, see: https://wiki.archlinux.org/title/Pacman  #
+################################################################
+# MIT Licensed (C) Alicia Sykes 2022 <https://aliciasykes.com> #
+################################################################
+
+# Apps to be installed via Pacman
+pacman_apps=(
+  # Essentials
+  'git'
+  'lazygit'
+  'tmux'
+  'wget'
+
+  # Terminal
+  'alacritty'
+
+  # Text / Code Editor, IDE
+  'neovim'
+
+  # CLI Power Basics
+  'aria2'
+  'bat'
+  'exa'
+  'fzf'
+  'fd'
+  'jq'
+  'ripgrep'
+  'sd'
+  'thefuck'
+  'tree'
+  'tokei'
+  # 'trash-cli'
+  # 'xsel'
+  'zoxide'
+  'yazi'
+  'unzip'
+
+  # Development, languages, interpriters, compilers, etc
+  # 'go'
+  'rustup'
+  'python-pip'
+  'nodejs'
+  'npm'
+  'cmake'
+  'clang'
+
+  # Security Utilities
+  'gnupg'
+  'openssl'
+
+  # Monitoring, management and stats
+  'btop'
+  # 'speedtest-cli'
+
+  # CLI Fun
+  'cowsay'
+  'lolcat'
+  'fastfetch'
+
+  # Apps
+  'vlc'
+  'obs-studio'
+)
+
+# Colors
+PURPLE='\033[0;35m'
+YELLOW='\033[0;93m'
+CYAN_B='\033[1;96m'
+LIGHT='\x1b[2m'
+RESET='\033[0m'
+
+PROMPT_TIMEOUT=15 # When user is prompted for input, skip after x seconds
+
+# If set to auto-yes - then don't wait for user reply
+if [[ $* == *"--auto-yes"* ]]; then
+  PROMPT_TIMEOUT=0
+  REPLY='Y'
+fi
+
+# Print intro message
+echo -e "\n${PURPLE}Starting Arch package install / update script"
+echo -e "${LIGHT}The following script is for Arch / Arch-based headless systems, and will"
+echo -e "update database, upgrade packages, clear cache then install all listed CLI apps."
+echo -e "${YELLOW}Before proceeding, ensure your happy with all the packages listed in \e[4m${0##*/}"
+echo -e "${RESET}"
+
+# Check if running as root, and prompt for password if not
+if [ "$EUID" -ne 0 ]; then
+  echo -e "${PURPLE}Elevated permissions are required to adjust system settings."
+  echo -e "${CYAN_B}Please enter your password...${RESET}"
+  sudo -v
+  if [ $? -eq 1 ]; then
+    echo -e "${YELLOW}Exiting, as not being run as sudo${RESET}"
+    exit 1
+  fi
+fi
+
+# Check pacman actually installed
+if ! hash pacman 2> /dev/null; then
+  echo "${YELLOW_B}Pacman doesn't seem to be present on your system. Exiting...${RESET}"
+  exit 1
+fi
+
+# Prompt user to update package database
+echo -e "${CYAN_B}Would you like to update package database? (y/N)${RESET}\n"
+read -t $PROMPT_TIMEOUT -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  echo -e "${PURPLE}Updating database...${RESET}"
+  sudo pacman -Syy --noconfirm
+fi
+
+# Prompt user to upgrade currently installed packages
+echo -e "${CYAN_B}Would you like to upgrade currently installed packages? (y/N)${RESET}\n"
+read -t $PROMPT_TIMEOUT -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  echo -e "${PURPLE}Upgrading installed packages...${RESET}"
+  sudo pacman -Syu --noconfirm
+fi
+
+# Prompt user to clear old package caches
+echo -e "${CYAN_B}Would you like to clear unused package caches? (y/N)${RESET}\n"
+read -t $PROMPT_TIMEOUT -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  echo -e "${PURPLE}Freeing up disk space...${RESET}"
+  sudo pacman -Sc --noconfirm
+  paccache -r
+fi
+
+# Prompt user to install all listed apps
+echo -e "${CYAN_B}Would you like to install listed apps? (y/N)${RESET}\n"
+read -t $PROMPT_TIMEOUT -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  echo -e "${PURPLE}Starting install...${RESET}"
+  for app in ${pacman_apps[@]}; do
+    if hash "${app}" 2> /dev/null; then
+      echo -e "${YELLOW}[Skipping]${LIGHT} ${app} is already installed${RESET}"
+    elif [[ $(echo $(pacman -Qk $(echo $app | tr 'A-Z' 'a-z') 2> /dev/null )) == *"total files"* ]]; then
+      echo -e "${YELLOW}[Skipping]${LIGHT} ${app} is already installed via Pacman${RESET}"
+    elif hash flatpak 2> /dev/null && [[ ! -z $(echo $(flatpak list --columns=ref | grep $app)) ]]; then
+      echo -e "${YELLOW}[Skipping]${LIGHT} ${app} is already installed via Flatpak${RESET}"
+    else
+      echo -e "${PURPLE}[Installing]${LIGHT} Downloading ${app}...${RESET}"
+      sudo pacman -S ${app} --needed --noconfirm
+    fi
+  done
+fi
+
+echo -e "${PURPLE}Finished installing / updating Arch packages.${RESET}"
+
+# EOF
