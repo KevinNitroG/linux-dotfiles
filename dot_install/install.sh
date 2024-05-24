925a7f9fd8dc689e0aa8420894642f4528f7e33d
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-05-24 18:01:57 +0700

    🧹 chore(save): Copy pasta fail. still had a instead of a

--- dot_install/install.sh
+++ dot_install/install.sh
@@ -3,25 +3,17 @@
 read -p "MAKE SURE YOU RUN THIS SCRIPT IN USER PRIVILEDGE? (y/N) " user_input
 
 case $user_input in 
-  [yY] )
-    continue
-    ;;
+  [yY] ) continue ;;
   [nN] )
-  * ) 
-    exit 1
-    ;;
+  * ) exit 1 ;;
 esac
 
 read -p "MAKE SURE YOU HAVE IMPORTED SSH, GPG KEY? (Y/n) " user_input
 
 case $user_input in 
-  [nN] )
-    exit 1
-    ;;
+  [nN] ) exit 1 ;;
   [yY] )
-  * ) 
-    continue
-    ;;
+  * ) continue ;;
 esac
 
 # Install packages
@@ -39,6 +31,10 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/KevinNitroG/linux-dotfiles
 # Chezmoi
 chezmoi init --apply --verbose git@github.com:KevinNitroG/linux-dotfiles.git
 
+# Laptop sutff
+echo "INSTALLING LAPTOP STUFF..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/KevinNitroG/linux-dotfiles/main/dot_install/laptop_stuff.sh)"
+
 # TPM for tmux
 git clone https://github.com/tmux-plugins/tpm /.tmux/plugins/tpm
 
@@ -49,14 +45,11 @@ fi
 read -p "INSTALL HYPRDOTS? (Y/n) " user_input
 
 case $user_input in 
-  [nN] )
-    continue
-    ;;
+  [nN] ) continue ;;
 	[yY] )
-  * ) 
+  * )
     git clone --depth 1 https://github.com/prasanthrangan/hyprdots ~/HyDE
     cd ~/HyDE/Scripts
-    ./install.sh
-    ;;
+    ./install.sh ;;
 esac
 
--- dot_install/laptop_stuff.sh
+++ dot_install/laptop_stuff.sh
@@ -2,3 +2,6 @@
 
 pacman -S tlp --no-confirm
 yay -S tlpui --no-confirm
+
+sudo rm /etc/tlp.conf || true
+sudo ln -s ~/.config/tlp/tlp.conf /etc/tlp.conf
