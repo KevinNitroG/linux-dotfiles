d8cc6fdc151dd7f836f02e0ab52e1664cf53e832
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-07-13 22:16:54 +0700

    feat(install): add `ttf-google-sans`

--- dot_install/yay_packages.sh
+++ dot_install/yay_packages.sh
@@ -3,10 +3,11 @@
 pacman -S --needed git base-devel yay
 
 yay_packages=(
-	# 'authy-desktop-win32-bin'
-	'google-chrome'
-	'spotify'
-	'visual-studio-code-bin'
+  # 'authy-desktop-win32-bin'
+  'google-chrome'
+  'spotify'
+  'visual-studio-code-bin'
+  'ttf-google-sans'
 )
 
 yay -S $yay_packages --no-confirm
