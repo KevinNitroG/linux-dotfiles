3a8931837b128b55ee3024edd956d9b6ffe823bb
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-07-15 18:20:23 +0700

    feat(scripts): curl || wget for update catppuccin stylus

--- dot_scripts/executable_catppuccin-stylus-update.sh
+++ dot_scripts/executable_catppuccin-stylus-update.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-url="https://github.com/catppuccin/userstyles/releases/download/all-userstyles-export/import.json"
+URL="https://github.com/catppuccin/userstyles/releases/download/all-userstyles-export/import.json"
+FILE_DIR="$HOME/.config/stylus"
+FILE_PATH="$FILE_DIR/catppuccin.json"
 
-file="$HOME/.config/stylus/catppuccin.json"
+mkdir -p "$FILE_DIR"
 
-wget -qO "$file" "$url"
+if curl -V &>/dev/null; then
+  curl -L -o "$FILE_PATH" "$URL"
+elif wget -V &>/dev/null; then
+  wget -qO "$FILE_PATH" "$URL"
+fi
 
-sed -i 's/"name":"accentColor","value":null/"name":"accentColor","value":"lavender"/g' "$file"
+sed -i 's/"name":"accentColor","value":null/"name":"accentColor","value":"lavender"/g' "$FILE_PATH"
