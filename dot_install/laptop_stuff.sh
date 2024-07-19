0b3d9cef7ca43a3d438e4506544b04e5dbf6711e
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-07-19 11:34:27 +0700

    feat(install/config): add `libinput-gestures`

--- dot_install/laptop_stuff.sh
+++ dot_install/laptop_stuff.sh
@@ -1,7 +1,19 @@
 #/bin/bash
 
-pacman -S tlp --no-confirm
-yay -S tlpui --no-confirm
+pacman_packages=(
+  tlp
+)
+
+yay_packages=(
+  tlpui
+  libinput-gestures
+)
+
+pacman -S $pacman_packages --no-confirm
+yay -S $yay_packages --no-confirm
 
 sudo rm /etc/tlp.conf || true
 sudo ln -s ~/.config/tlp/tlp.conf /etc/tlp.conf
+
+# [re]start and setup auto start for libinput-gestures
+libinput-gestures-setup stop desktop autostart start
