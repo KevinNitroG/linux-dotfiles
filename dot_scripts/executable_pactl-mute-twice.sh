6609d7234b2dab9540f59e61e7f09e09071433ef
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-08-05 18:48:20 +0700

    chore(scripts): pactl check

--- dot_scripts/executable_pactl-mute-twice.sh
+++ dot_scripts/executable_pactl-mute-twice.sh
@@ -2,5 +2,10 @@
 
 # Mute and unmute to make the audio work
 
+if ! command -v pactl >/dev/null 2>&1; then
+  echo "pactl isn't installed"
+  exit 1
+fi
+
 pactl set-sink-mute 0 toggle
 pactl set-sink-mute 0 toggle
